031fe4e4d8b521ff1dbc80a3236ded5f2af4250c
Zokirjon01 <sizning_emailingiz@example.com>
2025-09-19 22:32:56 +0500

    ALLOWED_HOSTS updated

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret_key")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["dasturlash-olami.onrender.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',

2355dbd7734fd3631460a17854138c3d7a8090c3
Zokirjon01 <sizning_emailingiz@example.com>
2025-09-20 14:01:31 +0500

    Updated settings

--- config/settings.py
+++ config/settings.py
@@ -57,14 +57,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database (PostgreSQL ulanishi)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    }
+    'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
 
 AUTH_PASSWORD_VALIDATORS = [

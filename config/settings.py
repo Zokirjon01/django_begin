830c57244efbb068af286c88902dd3e41d7ed8d4
Zokirjon <zokirjonaxmatov998@mail.ru>
2026-01-17 14:07:24 +0500

    Fix postgres ssl + requirements

--- config/settings.py
+++ config/settings.py
@@ -55,10 +55,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-# Postgres DATABASE
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
     )
